026399f11e61dbb94a8a87e653c7b5e4572329cc
bryanmelendez <bryanmelendez2323@gmail.com>
2024-08-15 00:50:11 -0700

    changed lualine to square separators

--- nvim/.config/nvim/after/plugin/lualine.lua
+++ nvim/.config/nvim/after/plugin/lualine.lua
@@ -1,1 +1,11 @@
-require('lualine').setup()
+local lualine = require('lualine')
+
+local config = {
+    options = {
+        -- Disable sections and component separators
+        component_separators={left='|',right='|'},
+        section_separators={left='',right=''}, 
+    }
+}
+
+lualine.setup(config)
