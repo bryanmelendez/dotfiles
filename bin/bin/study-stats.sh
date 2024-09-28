34b5d5e8f487c3b3af7f78e1df2ef1125a46f0e7
bryanmelendez <bryanmelendez2323@gmail.com>
2024-09-27 22:37:15 -0700

    study log

--- bin/bin/study-stats.sh
+++ bin/bin/study-stats.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 LOG_PATH="$HOME/projects/study-log"
-SAVE_PATH="$notes/1-personal/study-log"
+SAVE_PATH="$notes/spaces/life/study-log"
 
 # Copy the current study stats save to the program directory
 cp -v $SAVE_PATH/study_stats.md $LOG_PATH
 mv -v $LOG_PATH/study_stats.md $LOG_PATH/study_stats.yaml
 # Copy the current study stats save to my archives
-cp -v $SAVE_PATH/study_stats.md $notes/4-archives/
+cp -v $SAVE_PATH/study_stats.md $notes/.trash
 # run the program
 cd $LOG_PATH
 source venv/bin/activate
